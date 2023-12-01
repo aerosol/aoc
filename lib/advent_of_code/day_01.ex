74a1a8001cdec094a6b6eeeb7852f1495125ea02
Adam Rutkowski <hq@mtod.org>
2023-12-01 15:27:20 +0100

    p2: get a failing test at last

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -16,6 +16,43 @@ defmodule AdventOfCode.Day01 do
     for <<x <- s>>, x in ?0..?9, do: x - 48
   end
 
-  def part2(_args) do
+  @number_words ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  def(part2(input)) do
+    input
+    |> String.split("\n")
+    |> Enum.reduce(0, fn
+      x, acc when byte_size(x) > 0 ->
+        l = find_ints2(x)
+
+        acc + Integer.undigits(l)
+
+      _, acc ->
+        acc
+    end)
+  end
+
+  def find_ints2(<<>>, acc) do
+    [List.last(acc), List.first(acc)]
+  end
+
+  def find_ints2(<<x, rest::binary>>, acc) when x in ?0..?9 do
+    find_ints2(rest, [x - 48 | acc])
+  end
+
+  for {clause, int} <-
+        Enum.with_index(
+          @number_words,
+          1
+        ) do
+    def find_ints2(<<unquote(clause), rest::binary>>, acc),
+      do: find_ints2(rest, [unquote(int) | acc])
+  end
+
+  def find_ints2(<<_x, rest::binary>>, acc) do
+    find_ints2(rest, acc)
+  end
+
+  def find_ints2(x) do
+    find_ints2(x, [])
   end
 end
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -10,11 +10,39 @@ defmodule AdventOfCode.Day01Test do
     assert result == 55029
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = """
+
+    two1nine
+    eightwothree
+
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+
+    """
+
     result = part2(input)
 
-    assert result
+    assert result == 281
+
+    input = """
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet
+    """
+
+    assert part2(input) == 142
+
+    assert part2("one1") == 11
+    assert part2("1one") == 11
+    assert part2("two1") == 21
+    assert part2("1two") == 12
+    assert part2("1two") == 12
+
+    assert part2("oneight") == 18
   end
 end
