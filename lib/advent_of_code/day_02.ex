03adcec6f3ad06072d6a96e904e975ba82fa55e9
Adam Rutkowski <hq@mtod.org>
2023-12-02 16:03:48 +0100

    d2 p1

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,5 +1,67 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  defmodule Parser do
+    import NimbleParsec
+
+    game_id =
+      string("Game ")
+      |> ignore()
+      |> integer(min: 1, max: 3)
+      |> ignore(string(": "))
+
+    cube_set =
+      times(
+        integer(min: 1, max: 3)
+        |> ignore(ascii_char([?\s]))
+        |> choice([string("red"), string("green"), string("blue")])
+        |> ignore(optional(ascii_char([?,])))
+        |> ignore(optional(ascii_char([?\s]))),
+        min: 1,
+        max: 3
+      )
+      |> reduce(:cube_set_reducer)
+      |> ignore(optional(string("; ")))
+
+    cube_sets = repeat(cube_set)
+
+    game =
+      game_id
+      |> wrap(cube_sets)
+      |> ignore(optional(ascii_char([?\n])))
+      |> wrap()
+
+    games = repeat(game)
+
+    defparsec(:game_id, game_id)
+    defparsec(:cube_set, cube_set)
+    defparsec(:cube_sets, cube_sets)
+    defparsec(:game, game)
+    defparsec(:games, games)
+
+    def cube_set_reducer(x) do
+      for [k, v] <- Enum.chunk_every(x, 2), do: {v, k}, into: %{}
+    end
+  end
+
+  def parse!(input, parsec \\ :games) do
+    {:ok, result, "", %{}, _, _} = apply(Parser, parsec, [input])
+    result
+  end
+
+  def part1(input, bag \\ %{}) do
+    input
+    |> parse!()
+    |> Enum.filter(&game_possible?(&1, bag))
+    |> Enum.map(fn [game_id, _] -> game_id end)
+    |> Enum.sum()
+  end
+
+  def game_possible?([_game_id, cube_sets], bag) do
+    any_exceeding? =
+      Enum.find(cube_sets, fn cube_set ->
+        Enum.any?(cube_set, fn {k, v} -> is_nil(bag[k]) or bag[k] < v end)
+      end)
+
+    !any_exceeding?
   end
 
   def part2(_args) do
--- lib/mix/tasks/d02.p1.ex
+++ lib/mix/tasks/d02.p1.ex
@@ -5,13 +5,15 @@ defmodule Mix.Tasks.D02.P1 do
 
   @shortdoc "Day 02 Part 1"
   def run(args) do
-    input = nil
+    input = AdventOfCode.Input.get!(2)
+
+    bag = %{"red" => 12, "green" => 13, "blue" => 14}
 
     if Enum.member?(args, "-b"),
-      do: Benchee.run(%{part_1: fn -> input |> part1() end}),
+      do: Benchee.run(%{part_1: fn -> input |> part1() end}, profile_after: true),
       else:
         input
-        |> part1()
+        |> part1(bag)
         |> IO.inspect(label: "Part 1 Results")
   end
 end
