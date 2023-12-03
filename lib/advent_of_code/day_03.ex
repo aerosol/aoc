da02bbad2093538a643656af57637a3ae5f4fdef
Adam Rutkowski <hq@mtod.org>
2023-12-03 11:34:06 +0100

    d3 p1

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,5 +1,88 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  defmodule Parser do
+    import NimbleParsec
+
+    number =
+      integer(min: 1, max: 3)
+      |> post_traverse({:emit, [:number]})
+
+    dot = ascii_char([?.]) |> ignore()
+
+    symbol =
+      ascii_char([{:not, ?.}, {:not, ?\n}])
+      |> post_traverse({:emit, [:symbol]})
+
+    schematic =
+      repeat(
+        repeat(choice([number, dot, symbol]))
+        |> ignore(ascii_char([?\n]))
+      )
+
+    def emit(rest, [number], context, {line, pos}, offset, :number = type) do
+      points =
+        for i <- 1..length(Integer.digits(number)) do
+          {line, offset - pos - i + 1}
+        end
+
+      {rest, [{type, points, number}], context}
+    end
+
+    def emit(rest, args, context, {line, pos}, offset, :symbol = type) do
+      {rest, [{type, [{line, offset - pos}], args}], context}
+    end
+
+    defparsec(:schematic, schematic)
+  end
+
+  def parse_schematic!(schematic) do
+    {:ok, schematic, "", %{}, _, _} = Parser.schematic(schematic)
+    schematic
+  end
+
+  def matrix(schematic) do
+    Enum.reduce(schematic, %{}, fn
+      {:symbol, [point], value}, matrix ->
+        Map.put(matrix, point, [{:symbol, value}])
+
+      {:number, points, value}, matrix ->
+        Enum.reduce(points, matrix, fn point, matrix ->
+          Map.put(matrix, point, [{:number, value}])
+        end)
+    end)
+  end
+
+  def symbol_adjacent?(points, matrix) do
+    Enum.any?(points, fn {x, y} ->
+      [
+        {x - 1, y},
+        {x, y - 1},
+        {x - 1, y - 1},
+        {x + 1, y},
+        {x, y + 1},
+        {x + 1, y + 1},
+        {x + 1, y - 1},
+        {x - 1, y + 1}
+      ]
+      |> Enum.reduce_while(false, fn point, _acc ->
+        case matrix[point] do
+          [symbol: _symbol] -> {:halt, true}
+          _ -> {:cont, false}
+        end
+      end)
+    end)
+  end
+
+  def part1(input) do
+    schematic = parse_schematic!(input)
+    matrix = matrix(schematic)
+
+    Enum.reduce(schematic, 0, fn
+      {:number, points, value}, acc ->
+        if symbol_adjacent?(points, matrix), do: acc + value, else: acc
+
+      _, acc ->
+        acc
+    end)
   end
 
   def part2(_args) do
--- test/advent_of_code/day_03_test.exs
+++ test/advent_of_code/day_03_test.exs
@@ -2,13 +2,74 @@ defmodule AdventOfCode.Day03Test do
   use ExUnit.Case
 
   import AdventOfCode.Day03
+  import AdventOfCode.Day03.Parser
 
-  @tag :skip
+  describe "parse" do
+    test "parse schematic" do
+      input = """
+      3..
+      .$.
+      127
+      """
+
+      assert [
+               {:number, [{1, 1}], 3},
+               {:symbol, [{2, 2}], '$'},
+               {:number, [{3, 3}, {3, 2}, {3, 1}], 127}
+             ] = parse_schematic!(input)
+    end
+
+    test "matrix" do
+      assert %{
+               {1, 1} => [number: 3],
+               {2, 2} => [symbol: '$'],
+               {3, 3} => [number: 127],
+               {3, 2} => [number: 127],
+               {3, 1} => [number: 127]
+             } =
+               matrix([
+                 {:number, [{1, 1}], 3},
+                 {:symbol, [{2, 2}], '$'},
+                 {:number, [{3, 3}, {3, 2}, {3, 1}], 127}
+               ])
+    end
+
+    test "symbol_adjacent?" do
+      input = """
+      3..
+      .$.
+      127
+      1..
+      """
+
+      schematic = parse_schematic!(input)
+      matrix = matrix(schematic)
+
+      assert symbol_adjacent?([{1, 1}], matrix)
+      assert symbol_adjacent?([{3, 1}], matrix)
+      assert symbol_adjacent?([{3, 2}], matrix)
+      assert symbol_adjacent?([{3, 3}], matrix)
+      refute symbol_adjacent?([{4, 1}], matrix)
+    end
+  end
+
   test "part1" do
-    input = nil
+    input = """
+    467..114..
+    ...*......
+    ..35..633.
+    ......#...
+    617*......
+    .....+.58.
+    ..592.....
+    ......755.
+    ...$.*....
+    .664.598..
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 3014
   end
 
   @tag :skip
