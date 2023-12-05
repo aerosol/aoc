20e5c939882ce1bba286053050044b1e781ac090
Adam Rutkowski <hq@mtod.org>
2023-12-05 16:22:56 +0100

    d5 p1

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,5 +1,119 @@
 defmodule AdventOfCode.Day05 do
-  def part1(_args) do
+  defmodule Parser do
+    import NimbleParsec
+
+    seeds =
+      string("seeds: ")
+      |> ignore()
+      |> repeat(
+        integer(min: 1)
+        |> ignore(optional(ascii_char([?\s])))
+      )
+      |> ignore(ascii_char([?\n]))
+      |> tag(:seeds)
+
+    component =
+      choice([
+        string("seed"),
+        string("soil"),
+        string("fertilizer"),
+        string("water"),
+        string("light"),
+        string("temperature"),
+        string("humidity"),
+        string("location")
+      ])
+
+    conversion_header =
+      component
+      |> unwrap_and_tag(:source)
+      |> concat(ignore(string("-to-")))
+      |> concat(component |> unwrap_and_tag(:destination))
+      |> ignore(string(" map:\n"))
+
+    conversion =
+      conversion_header
+      |> concat(
+        repeat(
+          integer(min: 1)
+          |> unwrap_and_tag(:dst_range_start)
+          |> concat(
+            ignore(ascii_char([?\s]))
+            |> integer(min: 1)
+            |> unwrap_and_tag(:src_range_start)
+            |> ignore(ascii_char([?\s]))
+            |> concat(
+              integer(min: 1)
+              |> unwrap_and_tag(:range_len)
+            )
+            |> ignore(optional(ascii_char([?\n])))
+          )
+          |> wrap()
+        )
+        |> tag(:meta)
+      )
+      |> ignore(optional(ascii_char([?\n])))
+
+    conversions =
+      repeat(conversion |> wrap())
+      |> tag(:conversions)
+
+    almanac =
+      seeds
+      |> ignore(ascii_char([?\n]))
+      |> concat(conversions)
+
+    defparsec(:seeds, seeds)
+    defparsec(:conversion_header, conversion_header)
+    defparsec(:conversion, conversion)
+    defparsec(:almanac, almanac)
+  end
+
+  def parse(input, parsec \\ :almanac) do
+    {:ok, result, "", %{}, _, _} = apply(Parser, parsec, [input])
+    result
+  end
+
+  def find(source, src_c, dst_c, almanac) do
+    meta =
+      almanac[:conversions]
+      |> Enum.find(&(&1[:source] == src_c and &1[:destination] == dst_c))
+      |> Keyword.fetch!(:meta)
+      |> Enum.find(&(source in &1[:src_range_start]..(&1[:src_range_start] + &1[:range_len] - 1)))
+
+    if meta do
+      offset = source - meta[:src_range_start]
+      meta[:dst_range_start] + offset
+    else
+      source
+    end
+  end
+
+  def find_till_location(seed, almanac) do
+    seed
+    |> find("seed", "soil", almanac)
+    |> find("soil", "fertilizer", almanac)
+    |> find("fertilizer", "water", almanac)
+    |> find("water", "light", almanac)
+    |> find("light", "temperature", almanac)
+    |> find("temperature", "humidity", almanac)
+    |> find("humidity", "location", almanac)
+  end
+
+  def part1(input) do
+    almanac = input |> parse()
+
+    almanac
+    |> Keyword.fetch!(:seeds)
+    |> Enum.reduce(nil, fn seed, lowest ->
+      location = find_till_location(seed, almanac)
+
+      if is_nil(lowest) or location < lowest do
+        location
+      else
+        lowest
+      end
+    end)
   end
 
   def part2(_args) do
--- test/advent_of_code/day_05_test.exs
+++ test/advent_of_code/day_05_test.exs
@@ -3,12 +3,101 @@ defmodule AdventOfCode.Day05Test do
 
   import AdventOfCode.Day05
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  test "parse" do
+    # # parse("seeds: 12 37 8 9\n", :seeds)
+    # # |> IO.inspect(label: :seeds)
 
-    assert result
+    # # parse("seed-to-soil map:\n", :conversion_header)
+    # # |> IO.inspect(label: :header)
+
+    # parse(
+    #   """
+    #   seed-to-soil map:
+    #   50 98 2
+    #   300 500 480
+    #   """,
+    #   :conversion
+    # )
+    # |> IO.inspect(label: :conversion)
+
+    input = """
+    seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+
+    water-to-light map:
+    88 18 7
+    18 25 70
+
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+
+    humidity-to-location map:
+    60 56 37
+    56 93 4
+    """
+
+    parse(input)
+    |> IO.inspect(label: :parsed)
+  end
+
+  test "part1" do
+    input = """
+    seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+
+    water-to-light map:
+    88 18 7
+    18 25 70
+
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+
+    humidity-to-location map:
+    60 56 37
+    56 93 4
+    """
+
+    assert part1(input) == 35
   end
 
   @tag :skip
